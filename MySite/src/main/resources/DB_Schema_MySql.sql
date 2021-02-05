8daddb2f9d115e29c785c618b59de761546c7e7d
Chris <chris1108g@gmail.com>
2021-02-05 18:17:16 +0900

    Database table schema file

---
+++ MySite/src/main/resources/DB_Schema_MySql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `tag_info` (
+  `tagNo` int NOT NULL AUTO_INCREMENT,
+  `tagName` varchar(45) NOT NULL,
+  PRIMARY KEY (`tagNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
